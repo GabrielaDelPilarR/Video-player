5f82c3607a0ad03ad4f77d4abd781799430a0012
Gabriela del Pilar Rojas <77763655+GabrielaDelPilarR@users.noreply.github.com>
2021-07-07 23:10:29 -0500

    js-js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+const $video = document.querySelector('#video')
+const $play = document.querySelector('#play')
+const $pause = document.querySelector('#pause')
+const $backward = document.querySelector('#backward')
+const $forward = document.querySelector('#forward')
+
+$play.addEventListener('click', handleplay)
+function handleplay() {
+  $video.play()
+  $play.hidden = true
+  $pause.hidden = false
+  console.log('le diste click al boton de play')
+}
+
+
+$pause.addEventListener('click', handlepause)
+function handlepause() {
+  $video.pause()
+  $pause.hidden = true
+  $play.hidden = false
+  console.log('le diste click al boton de pause')
+}
+
+$backward.addEventListener('click', handleBackward)
+function handleBackward() {
+  $video.currenTime= $video.currenTime - 10
+  console.log('le diste click al boton de backward' , $video.currenTime)
+}
+
+$forward.addEventListener('click', handleforward)
+function handleforward() {
+  $video.currenTime = $video.currenTime + 10
+  console.log('le diste click al boton de forward', $video.currenTime)
+}
+
